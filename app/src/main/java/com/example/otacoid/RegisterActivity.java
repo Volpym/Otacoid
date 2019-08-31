0e3d98a7ad418991b5f0de7c91d53840eb817aad
Vasilis Alepopoulos <vasilisalepopoulos@gmail.com>
2019-08-31 13:32:53 +0300

    Changed the code to start using AsyncTask for registration

--- app/src/main/java/com/example/otacoid/RegisterActivity.java
+++ app/src/main/java/com/example/otacoid/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.otacoid;
 
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,43 +13,41 @@ import android.widget.Toast;
 import com.example.otacoid.database.User;
 
 public class RegisterActivity extends AppCompatActivity{
-    User user = new User();
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
 
-        Button bSignUpButton = findViewById(R.id.buttonSignUp);
+        Button bSignUpButton = findViewById(R.id.buttonSignIn);
         final EditText eUsername = findViewById(R.id.editUsernameText);
         final EditText ePassword = findViewById(R.id.editPasswordText);
         final EditText eEmail  = findViewById(R.id.editEmailText);
 
-
-
         bSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                final String username = eUsername.getText().toString();
-                final String password = ePassword.getText().toString();
-                final String email = eEmail.getText().toString();
-
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        user.register(username,password,email);
-                    }
-                }).start();
-                Context context = getApplicationContext();
-                CharSequence text = "Registered!";
-                int duration = Toast.LENGTH_SHORT;
-
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-
+                new Register_Async().execute();
             }
 
-        });
+
+    });}
+
+
+        private class Register_Async extends AsyncTask<Void, Void, Void>{
+        User user = new User();
+        final EditText eUsername = findViewById(R.id.editUsernameText);
+        final EditText ePassword = findViewById(R.id.editPasswordText);
+        final EditText eEmail  = findViewById(R.id.editEmailText);
+        final String username = eUsername.getText().toString();
+        final String password = ePassword.getText().toString();
+        final String email = eEmail.getText().toString();
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            user.register(username,password,email);
+            return null;
+        }
     }
 
 }
